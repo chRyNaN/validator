df301a81d13605e3a9e609c1d5f753512a04b2a0
Chris <ckeenan@starry.com>
2020-09-10 14:39:33 -0400

    Created buildSrc module with LibraryConstants object

---
+++ buildSrc/src/main/java/LibraryConstants.kt
@@ -0,0 +1,14 @@
+package com.chrynan.validator.buildSrc
+
+object LibraryConstants {
+
+    const val bintrayRepo = "chrynan"
+    const val group = "com.chrynan.validator"
+    const val owner = "chrynan"
+    const val repoName = "validator"
+    const val versionName = "0.1.0"
+    const val versionCode = 1
+    const val versionDescription = "Pre-release 0.1.0"
+    const val license = "Apache-2.0"
+    const val vcsUrl = "https://github.com/chRyNaN/validator.git"
+}
