6bc8c6cabe2cc84dcbb232c3b72240c8bbe03921
Christopher Keenan <ckeenan@starry.com>
2022-02-28 22:08:18 -0600

    Updated build files to use Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import com.chrynan.validator.buildSrc.LibraryConstants
+
+group = LibraryConstants.group
+version = LibraryConstants.versionName
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://repo.repsy.io/mvn/chrynan/public") }
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.6.10")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.6.10")
+    }
+}
+
+apply(plugin = "org.jetbrains.dokka")
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://repo.repsy.io/mvn/chrynan/public") }
+    }
+}
+
+rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
+    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().nodeVersion = "16.0.0"
+}
+
+// Documentation
+tasks.named<org.jetbrains.dokka.gradle.DokkaMultiModuleTask>("dokkaGfmMultiModule").configure {
+    outputDirectory.set(file("${projectDir.path}/docs"))
+}
